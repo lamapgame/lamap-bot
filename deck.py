d813fb4c285a13ebe5f1aa6ee6163c3d9a8612f1
Dylan Tientcheu <dylanmambou17@gmail.com>
2020-05-04 19:37:25 +0100

    fix cards shuffle error

--- deck.py
+++ deck.py
@@ -6,11 +6,13 @@ from card import Card
 from errors import DeckEmptyError
 
 # LaMap's 31 playing cards
-LAMAP_CARDS = shuffle([
+LAMAP_CARDS = [
     'h_3', 'h_4', 'h_5', 'h_6', 'h_7', 'h_8', 'h_9', 'h_10', 'c_3', 'c_4', 'c_5', 'c_6',
     'c_7', 'c_8', 'c_9', 'c_10', 'd_3', 'd_4', 'd_5', 'd_6', 'd_7', 'd_8',
     'd_9', 'd_10', 's_3', 's_4', 's_5', 's_6', 's_7', 's_8', 's_9'
-])  # shuffle cards directly
+]  # shuffle cards directly
+
+shuffle(LAMAP_CARDS)
 
 
 class Deck(object):
