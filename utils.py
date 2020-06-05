b0da40cc337193f797d89b2320502c58d35aeb12
Dylan Tientcheu <dylanmambou17@gmail.com>
2020-06-05 23:35:54 +0100

    fix tuer le way error

--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ import logging
 from telegram.ext.dispatcher import run_async
 from global_variables import gm
 
+from mwt import MWT
+
 logger = logging.getLogger(__name__)
 
 TIMEOUT = 2.5
@@ -74,6 +76,12 @@ def user_is_creator_or_admin(user, game, bot, chat):
     return user_is_creator(user, game) or user_is_admin(user, bot, chat)
 
 
+def get_admin_ids(bot, chat_id):
+    """Returns a list of admin IDs for a given chat. Results are cached for 1 hour."""
+    return [admin.user.id for admin in bot.get_chat_administrators(chat_id)]
+
+
+@MWT(timeout=60*60)
 def get_admin_ids(bot, chat_id):
     """Returns a list of admin IDs for a given chat. Results are cached for 1 hour."""
     return [admin.user.id for admin in bot.get_chat_administrators(chat_id)]
