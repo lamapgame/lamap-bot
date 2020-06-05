75c1fced1a20685d7f0af8b7da44acf30bc24f55
Dylan Tientcheu <dylanmambou17@gmail.com>
2020-06-05 22:47:09 +0100

    add procfile

--- start_bot.py
+++ start_bot.py
@@ -1,2 +1,8 @@
+import os
+PORT = int(os.environ.get('PORT', 5000))
+HEROKU = 'https://afternoon-meadow-46045.herokuapp.com/'
+
+
 def start_bot(updater):
-    updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
+    updater.bot.setWebhook(HEROKU + TOKEN)
