b90835bb114282d499ef3acc4cd6f95a12fd30f5
Dylan Tientcheu <dylanmambou17@gmail.com>
2020-06-28 16:02:34 +0100

    🚀 add deployment db config

--- global_variables.py
+++ global_variables.py
@@ -1,10 +1,11 @@
-from config import TOKEN, WORKERS
+from config import TOKEN, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER, WORKERS, DB_NAME
 import logging
 from telegram.ext import Updater
 from game_manager import GameManager
 from database import db
 
-db.bind('sqlite', 'themap.sqlite', create_db=True)
+db.bind('postgres', user=DB_USER, password=DB_PASSWORD,
+        host=DB_HOST, database=DB_NAME, port=DB_PORT)
 db.generate_mapping(create_tables=True)
 
 updater = Updater(token=TOKEN, workers=WORKERS, use_context=True)
--- start_bot.py
+++ start_bot.py
@@ -1,13 +1,12 @@
-from config import TOKEN
+from config import TOKEN, HEROKU_URL
 import os
 import sys
 PORT = int(os.environ.get('PORT', 5000))
-HEROKU = 'https://afternoon-meadow-46045.herokuapp.com/'
 
 
 def start_bot(updater):
     if sys.argv[1] == "prod":
         updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
-        updater.bot.setWebhook(HEROKU + TOKEN)
+        updater.bot.setWebhook(HEROKU_URL + TOKEN)
     else:
         updater.start_polling()
