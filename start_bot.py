a41b6cb78fff47067f027ea4a870191061f090ed
Dylan Tientcheu <dylanmambou17@gmail.com>
2021-03-20 00:37:43 +0100

    add webhook support

--- start_bot.py
+++ start_bot.py
@@ -5,7 +5,6 @@ import sys
 def start_bot(updater):
     if sys.argv[1] == "prod":
         updater.start_webhook(
-            listen="0.0.0.0", port=HEROKU_PORT, url_path=TOKEN)
-        updater.bot.setWebhook(HEROKU_URL + TOKEN)
+            listen="0.0.0.0", port=HEROKU_PORT, url_path=TOKEN, webhook_url=HEROKU_URL + TOKEN)
     else:
         updater.start_polling()
