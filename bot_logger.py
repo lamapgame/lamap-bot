53d8012a249d90c4ecb7312e81cd14bd8fe5748e
Dylan Tientcheu <dylantientcheu@gmail.com>
2021-11-21 18:29:55 +0100

    ✨  remove web preview on admin logs

--- bot_logger.py
+++ bot_logger.py
@@ -5,4 +5,5 @@ from config import LOGGING_CHAT_ID
 
 def log_to_admin(update: Update, context: CallbackContext, message: str):
     """Logs the message to the logging chat"""
-    context.bot.send_message(chat_id=LOGGING_CHAT_ID, text=message)
+    context.bot.send_message(chat_id=LOGGING_CHAT_ID,
+                             text=message, disable_web_page_preview=True)
