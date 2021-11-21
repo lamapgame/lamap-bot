af3b5a8704b32a32ed234bff8fc5a8d502986d2f
Dylan Tientcheu <dylantientcheu@gmail.com>
2021-11-21 19:41:00 +0100

    update scheduler

--- scheduler.py
+++ scheduler.py
@@ -3,25 +3,31 @@ import database
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+@sched.scheduled_job('interval', seconds='30')
 def scheduled_job():
+    database.db.execute(
+        """UPDATE userdb
+        SET nkap = nkap + 10000
+        where nkap <= 100000 and verified = true
+        """
+    )
     print('This job is run every weekday at 5pm.')
 
 
-@sched.scheduled_job('cron', day_of_week='sun', hour=17)
+@sched.scheduled_job('cron', day_of_week='sun', hour=10)
 def share_nkap():
     database.db.execute(
         """UPDATE userdb
         SET nkap = (
             CASE
-            WHEN (nkap < 25000) THEN 25000
+            WHEN (nkap < 25000 and nkap > 0) THEN 25000
             WHEN (nkap < 0) THEN nkap + 25000
             END
         )
         where nkap <= 25000 and verified = true
         """
     )
-    print('time to share money')
+    print('-ADMIN- MONEY DROP')
 
 
 sched.start()
