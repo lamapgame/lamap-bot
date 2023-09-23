8c3dae692bb6377c450a74ab3b11b968b32f5ffc
Dylan Tientcheu <dylantientcheu@gmail.com>
2023-09-23 14:46:25 +0200

    fix: update format

--- orchestrator.py
+++ orchestrator.py
@@ -7,9 +7,11 @@ class Orchestrator:
     ## The Grand O
     - Manages **EVERY** game initiated on Telegram.
     - Communicates with players globally (warnings, upcoming games, ban status, money drops).
-    - Acts as a central point for systems to communicate about game events and state changes. For instance, the Orchestrator may notify the game manager when a player exits a game.
+    - Acts as a central point for systems to communicate about game events and state changes.
+    For instance, the Orchestrator may notify the game manager when a player exits a game.
     - Provides high-level insights on the bot's activity.
-    - Can be persistent, but not at the db level. It only monitors the current state of every game being played.
+    - Can be persistent, but not at the db level.
+    It only monitors the current state of every game being played.
     - Logs these insights for monitoring purposes or specialized logging.
     """
 
