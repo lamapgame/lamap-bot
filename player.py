2084c81c5a44537b7d0c594958e2b45151b0faaf
Dylan Tientcheu <dylantientcheu@gmail.com>
2023-09-23 15:05:28 +0200

    fix: lint issues

--- player.py
+++ player.py
@@ -2,9 +2,11 @@ from datetime import datetime
 
 from telegram import User
 
+from deck import Card
+
 
 class Player:
     def __init__(self, user: User, score: int):
         self.user = user
-        self.hand_of_cards = []
+        self.hand_of_cards: list[Card] = []
         self.turn_started_time = datetime.now()
