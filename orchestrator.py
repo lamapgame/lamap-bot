2dfe634e9b9fd3de074fd07d48e39ca1cff83370
Dylan Tientcheu <dylantientcheu@gmail.com>
2023-09-23 14:51:49 +0200

    fix: format

--- orchestrator.py
+++ orchestrator.py
@@ -4,15 +4,15 @@ from common.exceptions import GameAlreadyExistError
 
 class Orchestrator:
     """
-    ## The Grand O
-    - Manages **EVERY** game initiated on Telegram.
-    - Communicates with players globally (warnings, upcoming games, ban status, money drops).
-    - Acts as a central point for systems to communicate about game events and state changes.
-    For instance, the Orchestrator may notify the game manager when a player exits a game.
-    - Provides high-level insights on the bot's activity.
-    - Can be persistent, but not at the db level.
-    It only monitors the current state of every game being played.
-    - Logs these insights for monitoring purposes or specialized logging.
+    ## The Grand O:
+
+    - Manages all Telegram games.
+    - Communicates globally with players:
+        warnings, upcoming games, bans, money drops.
+    - Central hub for game event communication.
+    - Offers insights on bot activity.
+    - Monitors current game states; not database-persistent.
+    - Logs insights for monitoring and specialized logging.
     """
 
     def __init__(self):
