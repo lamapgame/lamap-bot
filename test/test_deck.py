f98686bdd070e45c13a631b5accd7ad78d9c958c
Dylan Tientcheu <dylantientcheu@gmail.com>
2023-12-20 12:19:57 +0100

    test: deck.py

--- test/test_deck.py
+++ test/test_deck.py
@@ -3,19 +3,51 @@ from deck import Deck, Card
 from player import Player
 
 
-def test_deck_initialization():
-    """tests that the deck is initialized with the correct number of cards"""
+def test_card_initialization():
+    """Test that a card is initialized with the correct attributes."""
+    card = Card("h", 5, "GALACTIC")
+    assert card.suit == "h"
+    assert card.value == 5
+    assert card.design == "GALACTIC"
+    assert card.icon == "♥️"
+
+
+def test_deck_shuffling():
+    """Test that the deck is shuffled."""
+    deck1 = Deck()
+    deck2 = Deck()
+    # There's a small chance this could fail due to randomness
+    assert deck1.cards != deck2.cards
+
+
+def test_special_card_computation():
+    """Test computation of special cards based on a hand."""
     deck = Deck()
-    assert isinstance(deck.cards, list)
-    assert len(deck.cards) >= 31
-    assert all(isinstance(card, Card) for card in deck.cards)
+    hand = [Card("h", 3, "DEFAULT"), Card("d", 3, "DEFAULT"), Card("s", 3, "DEFAULT")]
+    special_cards = deck.compute_cards(hand)
+    assert Card("x", 333, "DEFAULT") in special_cards
 
 
-def test_deck_cut():
-    """test deck cutting cards"""
+def test_card_comparison():
+    """Test the comparison of two cards."""
+    card1 = Card("h", 5, "DEFAULT")
+    card2 = Card("h", 6, "DEFAULT")
+    assert Card.is_better_than(card2, card1)
+
+
+def test_cut_cards_with_special():
+    """Test deck cutting cards including special card computation."""
     deck = Deck()
     player = Player(mock_objects.user1)
     deck.cut_cards(player)
-    assert len(player.hand_of_cards) == 5
-    assert all(isinstance(card, Card) for card in player.hand_of_cards)
-    assert all(card not in deck.cards for card in player.hand_of_cards)
+    assert len(player.hand_of_cards) >= 5
+
+
+def test_card_equality_and_string():
+    """Test card equality and string representation."""
+    card1 = Card("h", 5, "DEFAULT")
+    card2 = Card("h", 5, "DEFAULT")
+    card3 = Card("d", 5, "DEFAULT")
+    assert card1 == card2
+    assert card1 != card3
+    assert str(card1) == "h_5"
